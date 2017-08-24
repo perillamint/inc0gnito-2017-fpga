795e79605d0256b5251070305b1d978d176e44bc
perillamint <perillamint@quendi.moe>
2017-08-24 11:02:37 +0900

    Update solve.sh

--- solve.sh
+++ solve.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 z3 inc0gnito.lsp | perl -p -e 's/Bool\n//g' | perl -p -e 's/[ ]+/ /g' > result.lsp
 cat result.lsp | grep define-fun | awk '{ print $2 " " $4 }' | sed -r 's/\)//' > result.txt
+cat result.txt | grep 'd[0-9]*' | sort --version-sort > data.txt
+cat result.txt | grep 'k[0-9]*' | sort --version-sort > key.txt
